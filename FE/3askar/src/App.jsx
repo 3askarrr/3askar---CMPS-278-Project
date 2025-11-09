da88bedcd6c94b6e8cc4efff4bb8a63dbc3e70fa
ameerabahrani <aaa301@mail.aub.edu>
2025-11-09 14:35:36 +0200

    fix

--- FE/3askar/src/App.jsx
+++ FE/3askar/src/App.jsx
@@ -1,17 +1,35 @@
-import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import FileGrid from "./components/FileGrid";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Box sx={{ display: "flex", height: "100vh", backgroundColor: "#f9fafb" }}>
-      <Sidebar />
-      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
-        <Navbar />
-        <FileGrid />
-      </Box>
-    </Box>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
